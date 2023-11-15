7333e1315f93adfd59e01ce439ebbf50eeffb0fe
Jaco Kok <kokjaco2@gmail.com>
2023-11-15 08:43:08 +0200

    Better swagger responses

--- src/KayordKit/Extensions/Swagger/CustomOperationsProcessor.cs
+++ src/KayordKit/Extensions/Swagger/CustomOperationsProcessor.cs
@@ -7,11 +7,13 @@ public class CustomOperationsProcessor : IOperationProcessor
 {
     public bool Process(OperationProcessorContext context)
     {
-        // string result = context.OperationDescription.Operation.OperationId;
-        // result = result.Replace("ApiFeatures", "");
-        // result = result.Replace("Api", "");
-        // result = result.Replace("Endpoint", "");
-        // context.OperationDescription.Operation.OperationId = result;
+        string result = context.OperationDescription.Operation.OperationId;
+        string name = Util.GetAssembly().FullName ?? "";
+        string featureName = name + "Features";
+        result = result.Replace(featureName, "");
+        result = result.Replace(name, "");
+        result = result.Replace("Endpoint", "");
+        context.OperationDescription.Operation.OperationId = result;
         return true;
     }
 }
